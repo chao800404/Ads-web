f5c7f24602b8b484db094c590080fca36cbb25de
Chao <a0921342997@gmail.com>
2021-12-30 18:04:28 +0800

    change css

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CompressionPlugin = require("compression-webpack-plugin");
 const ImageminWebpWebpackPlugin = require("imagemin-webp-webpack-plugin");
-const devMode = process.env.NODE_ENV !== "production";
+const devMode = false;
 
 module.exports = {
   //入口

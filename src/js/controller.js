c7cbbebb79f409839676e3aee9ee0699991f146e
Chao <a0921342997@gmail.com>
2022-01-01 20:41:43 +0800

    test\

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,10 +4,10 @@
 
 import "../scss/main.scss";
 // import Swiper JS
-import Swiper from "swiper/bundle";
+// import Swiper from "swiper/bundle";
 
-import "swiper/css";
-import "swiper/css/navigation";
+// import "swiper/css";
+// import "swiper/css/navigation";
 ////////////////////////////////////////////////////////
 import resultPageVeiw from "./view/resultPageVeiw";
 import * as model from "./model";
@@ -24,33 +24,33 @@ import homeconsultation from "./view/Home/homeconsultation";
 
 //////////////////////////////////////////////////////////////////
 
-const controPageResult = function () {
-  const hash = window.location.hash.slice(1);
-  model.CreateStateobject(hash);
-  if (hash === ANCHOR[0] || hash === "") {
-    controHomePage(model.state);
-  }
-};
+// const controPageResult = function () {
+//   const hash = window.location.hash.slice(1);
+//   model.CreateStateobject(hash);
+//   if (hash === ANCHOR[0] || hash === "") {
+//     controHomePage(model.state);
+//   }
+// };
 
-const controHomePage = function (data) {
-  const { demo, feature, ourteam, consultation, firm } = data;
-  homedemo.render(demo);
-  homedemo.addHandlerStart(homedemo.addScrollView);
-  homedemo.addHandlerReize(homedemo.resizeDemo);
-  homefeature.render(feature);
-  homefeature.addHandlerOptions(homefeature.changeOptionContent);
-  homefeature.createObserver("0px", 0.25);
-  homeourteam.render(ourteam);
-  homeourteam.addSwiper();
-  homeourteam.createObserver("0px", 0.2);
-  homeconsultation.render(consultation, firm);
-  homeconsultation.addSwiper();
-  homeconsultation.createObserver("0px", 0.2);
-};
+// const controHomePage = function (data) {
+//   const { demo, feature, ourteam, consultation, firm } = data;
+//   homedemo.render(demo);
+//   homedemo.addHandlerStart(homedemo.addScrollView);
+//   homedemo.addHandlerReize(homedemo.resizeDemo);
+//   homefeature.render(feature);
+//   homefeature.addHandlerOptions(homefeature.changeOptionContent);
+//   homefeature.createObserver("0px", 0.25);
+//   homeourteam.render(ourteam);
+//   homeourteam.addSwiper();
+//   homeourteam.createObserver("0px", 0.2);
+//   homeconsultation.render(consultation, firm);
+//   homeconsultation.addSwiper();
+//   homeconsultation.createObserver("0px", 0.2);
+// };
 
-const init = function () {
-  model.CreateStateobject(ANCHOR[0]);
-  resultPageVeiw.addHandlerPage(controPageResult);
-};
+// const init = function () {
+//   model.CreateStateobject(ANCHOR[0]);
+//   resultPageVeiw.addHandlerPage(controPageResult);
+// };
 
-init();
+// init();

bfb553c15041314f6a6dbb07d8876258b016fd33
Chao <a0921342997@gmail.com>
2022-01-13 07:41:57 +0800

    the fixed

--- src/js/view/Webdesign/webDemoView.js
+++ src/js/view/Webdesign/webDemoView.js
@@ -12,11 +12,9 @@ class WebDemo extends View {
     return `
        <div class="template_demo webDesign">
         <div class="template_demo-container webDesign_image webDesignDemo_an">
-        <lottie-player class="webDesign-svg" src="${svgUrl}"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"  loop  autoplay></lottie-player>
+          <lottie-player class="webDesign-svg" src="${svgUrl}"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"  loop  autoplay></lottie-player>
         </div>
-        <div class="webDesign_radar-img radar-img_an">
-          <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_ofocdzqf.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"  loop  autoplay></lottie-player>
-        </div>
+        
         <h1 class="heading-1--1 webDesign_title">
             <span>${this._organizeText(title, 0, 1)}</span>
             <span>${this._organizeText(title, 1, 2)}</span>
@@ -27,3 +25,7 @@ class WebDemo extends View {
 }
 
 export default new WebDemo();
+
+/* <div class="webDesign_radar-img radar-img_an">
+      <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_ofocdzqf.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"  loop  autoplay></lottie-player>
+</div> */
